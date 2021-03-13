edadeb16d4b5edb208596554c97c42d0d84cd630
unknown <anup.bhagwat7@gmail.com>
2021-03-13 12:33:02 +0530

    Initial commit

---
+++ gcp-demo-react-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to GCP (Google Cloud  Platform) from <br></br><br></br> MyJavaBlog - <a href='https://www.myjavablog.com/'>https://www.myjavablog.com/</a>
+    </div>
+  );
+}
+
+export default App;
