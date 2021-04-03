c63c6c24025ca82ac06eec251ecee4f8523e0040
unknown <anup.bhagwat7@gmail.com>
2021-04-03 05:38:07 +0530

    Spring boot app deployment on GKE

---
+++ gcp-demo-springboot-app/src/main/java/com/myjavablog/GcpDemoSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.myjavablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDemoSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDemoSpringbootAppApplication.class, args);
+	}
+
+}
